f2336ce3fa761a8de4b6f1f381da4b505d8fec8a
Sheik Athikhur Rahman <98006123+Athik-444@users.noreply.github.com>
2022-01-29 17:36:33 +0530

    #include<stdio.h> #include<math.h> int main() {     int N,M;     float average;     scanf("%d%d",&N,&M);     average=(float)(N+M)/2;     printf("%0.4f",average);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int N,M;
+    float average;
+    scanf("%d%d",&N,&M);
+    average=(float)(N+M)/2;
+    printf("%0.4f",average);
+    return 0;
+}
